f48be14262feeac0516cbbeaa43e6d6affb1b52d
peterkitonga <pkitonga.pk@gmail.com>
2023-05-16 18:14:45 +0300

    Add microfrontend app for the auth page

---
+++ apps/auth/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angularnxstarter-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'auth';
+}
---
+++ apps/auth/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angularnxstarter-auth-entry',
+  template: `<angularnxstarter-nx-welcome></angularnxstarter-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
